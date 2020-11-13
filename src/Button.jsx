a8f47db885cc75fa2fc8fc2b88ea8c35a8171473
shoaib <shoaibsiddqui345@gmail.com>
2020-11-13 21:59:14 +0500

    update

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './index.css'
 
-const arr  = [0,1,2,3,4,5,6,7,8,9,".","+","-","*","/","="]
+const arr  = [0,1,2,3,4,5,6,7,8,9,".","+","-","*","/","=","Clear"]
 
 
 
